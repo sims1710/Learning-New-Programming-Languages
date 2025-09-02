dcaf2be443c8927ea5e695a332bceed4753843d2
Simriti <simritib1@gmail.com>
2025-09-02 16:52:53 +0800

    Create if.lua

---
+++ Learning Lua/Conditionals & Logic/if.lua
@@ -0,0 +1,12 @@
+--[[
+if statements require two parts:
+1. a boolean expression — a variable or some code 
+that evaluates to a true or false value.
+
+2. a code block — the code that is executed if the 
+boolean expression is true (and is skipped otherwise).
+]]
+isHungry = true
+if isHungry then
+  print("I need food")
+end 
