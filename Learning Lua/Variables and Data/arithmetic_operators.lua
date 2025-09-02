cd859d05d5aebb8ef3eb8ef4587394a820ff28bb
Simriti <simritib1@gmail.com>
2025-09-02 15:33:37 +0800

    Create arithmetic_operators.lua

---
+++ Learning Lua/Variables and Data/arithmetic_operators.lua
@@ -0,0 +1,58 @@
+--[[
+Operators Overview:
+
+Addition:
+- Description: Adds two numbers.
+- Syntax: x + y
+- Example: 5 + 2 → 7
+
+Subtraction:
+- Description: Subtracts one number from another.
+- Syntax: x - y
+- Example: 5 - 2 → 3
+
+Multiplication:
+- Description: Multiplies two numbers.
+- Syntax: x * y
+- Example: 5 * 2 → 10
+
+Division:
+- Description: Divides one number by another.
+- Syntax: x / y
+- Example: 5 / 2 → 2.5
+
+Exponential:
+- Description: Raises one number to the power of another.
+- Syntax: x ^ y
+- Example: 5 ^ 2 → 25
+
+Remainder (Modulo):
+- Description: Gives the leftover value after division.
+- Syntax: x % y
+- Example: 5 % 2 → 1
+
+Negation:
+- Description: Reverses the sign of a number.
+- Syntax: -x
+- Example: -(3 + 2) → -5
+]]
+
+print("Ji-Eun's current cart total:")
+-- Discover and print Ji-Eun's current total below using the addition operator
+print(10.95 + 20.99)
+
+print("Luca's ice cream inventory:")
+-- Discover and print Luca's ice cream inventory below using the subtraction operator
+print(300-298)
+
+print("Today's Net Total for Billy's Hat Store:")
+-- Discover and print Billy's revenue below using the multiplication operator
+print(60*49)
+
+print("The cost of one of Sage's pizza: ")
+-- Discover and print the cost of one pizza below using the division operator
+print(96/8)
+
+print("Marbles leftover for Layla: ")
+-- Discover and print the remainder marble below using the modulo operator
+print(7%2)
