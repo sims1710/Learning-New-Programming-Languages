0c2f9415056213d2a516d94c5980e0d7c28f7e8c
Simriti <simritib1@gmail.com>
2025-09-02 16:58:12 +0800

    Create if_else.lua

---
+++ Learning Lua/Conditionals & Logic/if_else.lua
@@ -0,0 +1,15 @@
+-- Example 1
+isHungry = true
+if isHungry then
+  print("I need food")
+else 
+  print("I am full, thanks")
+end
+
+-- Example 2
+score = 81
+if score >= 80 then
+  print("You've passed")
+else 
+  print("You did not pass")
+end
