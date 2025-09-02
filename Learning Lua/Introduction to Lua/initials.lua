eb6f0c8c617cf89a4ccc224411410312c6c00c6b
Simriti <simritib1@gmail.com>
2025-09-02 14:50:14 +0800

    Create initials.lua

---
+++ Learning Lua/Introduction to Lua/initials.lua
@@ -0,0 +1,8 @@
+-- Trying to print my intials in Lua
+print("BBBB     SSS ")
+print("B    B  S   S")
+print("B    B  S    ")
+print("BBBB     SSS ")
+print("B    B      S")
+print("B    B  S   S")
+print("BBBB     SSS ")
