b648940a37507baf684de07e1cc5da735a247906
Simriti <simritib1@gmail.com>
2025-09-02 14:38:55 +0800

    Create multi-line_comments.lua

---
+++ Learning Lua/Introduction to Lua/multi-line_comments.lua
@@ -0,0 +1,20 @@
+print("Starting the program")
+
+--[[
+print("Self destruction in 10...")
+print("9...")
+print("8...")
+print("7...")
+print("6...")
+print("5...")
+print("4...")
+print("3...")
+print("2...")
+print("1...")
+]]
+
+--[[ 
+print("Disable me!")
+print("This shouldn't happen")
+print("Let the hacking begin...")
+]]
