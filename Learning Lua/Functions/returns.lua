78874bc34426c5eb8c335e7706e6aaf945707efc
Simriti <simritib1@gmail.com>
2025-09-03 11:13:35 +0800

    Update returns.lua

--- Learning Lua/Functions/returns.lua
+++ Learning Lua/Functions/returns.lua
@@ -21,3 +21,28 @@ total2 = getShoppingCartTotal(300, 1.1)
 total3 = getShoppingCartTotal(50, 1.5)
 finalTotal = total1 + total2 + total3
 print(finalTotal)
+
+-- Example 3:
+function getBoostedSpeed(baseSpeed)
+  if (baseSpeed < 10)
+    return baseSpeed * 5
+  end
+  return baseSpeed * 2 -- This runs if we didn't already return
+end
+
+-- Example 4:
+function trickyReturns(input)
+  print("a")
+  if input then
+    print("b")
+    return "foo!"
+  end
+  print("c")
+  if 2+2 == 4 then
+    print("d")
+    return "boo!"
+  end
+  print("e")
+end
+
+trickyReturns(false)
